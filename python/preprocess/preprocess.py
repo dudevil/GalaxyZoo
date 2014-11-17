6717bc90a6efd387a8de704cd890868057e974e4
Sergey HPC <ovcharenko.ser@gmail.com>
2014-11-17 22:43:36 +0300

    Fooling around.

--- python/preprocess/preprocess.py
+++ python/preprocess/preprocess.py
@@ -11,26 +11,27 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from functools import partial
 from sklearn.decomposition import RandomizedPCA
+from sklearn.feature_extraction.image import PatchExtractor, extract_patches_2d
 
 train_images_path = '/home/dudevil/prog/dmlabs/GalaxyZoo/data/raw/images_training_rev1'
+seed = 171114
 
 galaxy_files = [os.path.join(train_images_path, f)
                 for f in os.listdir(train_images_path)
                 if f.endswith('.jpg')]
 
-def crop_and_resize(image, crop_offset=0, resolution=64):
+def crop_and_resize(image, crop_offset=108, resolution=60):
     img = image[crop_offset:-crop_offset, crop_offset:-crop_offset]
     if not isinstance(resolution, tuple):
         resolution = (resolution, resolution)
     return misc.imresize(img, resolution)
 
-def split_patches(image, size=16):
-    x, y, d = image.shape
-    x_chunks = x/size
-    y_chunks = y/size
-    return [patch
-            for vsplt in np.vsplit(image, x_chunks)
-            for patch in np.hsplit(vsplt, y_chunks)]
+def splitPatches(image, size=32):
+    patch1 = image[:size, :size]
+    patch2 = image[-size:, :size]
+    patch3 = image[:size, -size:]
+    patch4 = image[-size:, -size:]
+    return np.array([patch1, patch2, patch3, patch4])
 
 def normalize(image):
     """
@@ -42,22 +43,20 @@ def normalize(image):
     """
     return (image - np.mean(image))/np.sqrt(np.var(image) + 10)
 
-def whiten(image):
-    pca = RandomizedPCA(whiten=True).fit(image)
+
+def plotImageGrid(images, x, y):
+    for i, image in enumerate(images):
+        plt.subplot(x, y, i+1)
+        plt.imshow(image)
+        plt.axis('off')
+    plt.show()
 
 
+image1 = misc.imread(galaxy_files[0])
+image1 = crop_and_resize(image1)
+plotImageGrid(splitPatches(image1), 2, 2)
 
-
-image_file = galaxy_files[0]
-image = misc.imread(image_file, flatten=False)
-
-splt = split_patches(crop_and_resize(image, crop_offset=108))
-splt = map(normalize, splt)
-
-for i, patch in enumerate(splt):
-    plt.subplot(4, 4, i+1)
-    plt.imshow(patch)
-    plt.axis('off')
-
-plt.show()
+image2 = misc.imread(galaxy_files[1])
+image2 = crop_and_resize(image2)
+plotImageGrid(splitPatches(image2), 2, 2)
 
