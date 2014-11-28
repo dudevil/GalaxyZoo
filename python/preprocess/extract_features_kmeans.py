3baa0c1b755b3274345a3ed368a2a97ebd00976a
Sergey Ovcharenko <ovcharenko.ser@gmail.com>
2014-11-28 22:07:10 +0300

    Switch to mini-batch K-means, should be faster. And fix bugs.

--- python/preprocess/extract_features_kmeans.py
+++ python/preprocess/extract_features_kmeans.py
@@ -1,8 +1,11 @@
+#!/home/dudevil/prog/dmlabs/GalaxyZoo/python/.env/bin/python
+
 """
  Main module for feature extraction using K-means
 """
 
 import os
+import time
 import numpy as np
 from scipy import misc
 import matplotlib.pyplot as plt
@@ -12,12 +15,19 @@ from sklearn.feature_extraction.image import extract_patches_2d
 import sklearn.cluster as cluster
 from sklearn.externals import joblib
 
+from memory_profiler import profile
+
 __author__ = 'dudevil'
 
 # The current working dir should be the project top-level directory
 
 seed = 211114
 np.random.seed(seed)
+start_time = 0
+
+
+def _logWithTimestamp(msg):
+    print('[%d] %s' % (int(time.time()) - start_time, msg))
 
 
 def crop_and_resize(image, crop_offset=108, resolution=64):
@@ -27,7 +37,7 @@ def crop_and_resize(image, crop_offset=108, resolution=64):
     return misc.imresize(img, resolution)
 
 
-def plotImageGrid(images, image_size=(), nrow=4, ncol=4):
+def plotImageGrid(images, image_size=(), nrow=None, ncol=None):
     """
     Helper function to plot a grid of images
 
@@ -40,6 +50,9 @@ def plotImageGrid(images, image_size=(), nrow=4, ncol=4):
         x, y = image_size
     else:
         x, y = images.shape[1:]
+    if not (nrow or ncol):
+        # if not specified claculate the grid size
+        nrow = ncol = np.ceil(np.sqrt(len(images)))
     plt.figure(figsize=(x * ncol, y * nrow))
     for i, image in enumerate(images):
         plt.subplot(nrow, ncol, i + 1)
@@ -70,9 +83,23 @@ def readImages(n_images=None, images_path='data/raw/images_training_rev1',
     return images
 
 
-def buildFeatureDictionary(images, n_patches=32, patch_size=(16, 16), save_pics=True):
+def _fileNameSuffix(base, n_patches, patch_size, n_samples='', n_centroids='', n_components=''):
+    """
+     Just a utility function to build a filename from input parameters
+    """
+    base = "%s_n_%d_np_%d_ps_%d" % (base, n_samples, n_patches, patch_size[0])
+    if n_centroids:
+        base += '_nc_%d' % n_centroids
+    if n_components:
+        base += '_ncomp_%d' % n_components
+    base += '.png'
+    return base
+
+@profile
+def buildFeatureDictionary(images, n_patches=10, patch_size=(16, 16), n_centroids=100, save_pics=True):
     # set up the array and split images to patches
     X = np.zeros((n_patches * len(images), patch_size[0] * patch_size[1]))
+    n_images = len(images)
     for i, image in enumerate(images):
         patches = extract_patches_2d(image, patch_size=patch_size, max_patches=n_patches)
         X[i * n_patches:i * n_patches + n_patches, ...] = patches.reshape((n_patches, -1))
@@ -84,43 +111,45 @@ def buildFeatureDictionary(images, n_patches=32, patch_size=(16, 16), save_pics=
         # get some random patches to plot
         sample_patches_ind = np.random.choice(X.shape[0], 100)
         plotImageGrid(X[sample_patches_ind, ...],
-                      image_size=(patch_size, patch_size), nrow=10, ncol=10)
-        plt.savefig('pictures/patches16_g_10.png')
+                      image_size=patch_size, nrow=10, ncol=10)
+        plt.savefig(_fileNameSuffix('pictures/patches', n_patches, patch_size, n_images))
 
     # PCA\Whitening
-    print("==== Starting PCA ====")
+    _logWithTimestamp("==== Starting PCA ====")
     # perform whitening
     # 42 components = 99% explained varience
     n_components = 42
     pca = RandomizedPCA(n_components=n_components, whiten=True, random_state=seed)
-    w_X = pca.fit_transform(X)
+    X = pca.fit_transform(X)
 
-    print("variance explained: %f" % np.sum(pca.explained_variance_ratio_[:n_components]))
-    print("==== PCA fitted =====")
+    _logWithTimestamp("variance explained: %f" % np.sum(pca.explained_variance_ratio_[:n_components]))
+    _logWithTimestamp("==== PCA fitted =====")
 
     if save_pics:
         # plot whitened patches after inverse transform
-        orig_X = pca.inverse_transform(w_X[sample_patches_ind, ...])
-        plotImageGrid(orig_X, image_size=(patch_size, patch_size), nrow=10, ncol=10)
-        plt.savefig('pictures/patches_whitened16_g_10.png')
+        plotImageGrid(pca.inverse_transform(X[sample_patches_ind, ...]),
+                      image_size=patch_size, nrow=10, ncol=10)
+        plt.savefig(_fileNameSuffix('pictures/patches', n_patches, patch_size,
+                                    n_images, n_components=n_components))
 
     # ##KMEANS
-    print("==== Starting K-Means====")
-    k_means = cluster.KMeans(n_clusters=10, n_jobs=8)
-    k_means.fit(w_X)
-    print("==== K-Means fitted ====")
+    _logWithTimestamp("==== Starting K-Means====")
+    k_means = cluster.MiniBatchKMeans(n_clusters=n_centroids)
+    k_means.fit(X)
+    _logWithTimestamp("==== K-Means fitted ====")
 
     # get centroids and transform them to original space
     D = pca.inverse_transform(k_means.cluster_centers_)
     if save_pics:
         # plot 100 random centroids
-        plotImageGrid(D, image_size=(16, 16), nrow=1, ncol=10)
-        plt.savefig('pictures/centroids_16_g_10.png')
+        plotImageGrid(D, image_size=patch_size)
+        plt.savefig(_fileNameSuffix('pictures/centroids', n_patches, patch_size,
+                                    n_images, n_centroids=n_centroids))
 
     # save the results of calculations for future use
-    np.savetxt('data/models/centroids_10.csv', D, delimiter=',')
-    joblib.dump(pca, 'data/models/pca_10.pkl')
-    return pca, D
+    np.savetxt('data/models/centroids_%d_%d.csv' % (n_images, n_centroids), D, delimiter=',')
+    joblib.dump(pca, 'data/models/pca_%d.pkl' % n_images)
+    return D
 
 
 def loadFeatureDict(dict_file='data/models/centroids_10.csv'):
@@ -134,7 +163,6 @@ def loadPCAtransform(pca_file='data/models/pca_10.pkl'):
 def extractFeatures(patch, D, soft=True):
     # compute Euclidian distance from patch to centroid
     z = np.sqrt(np.sum(np.square(D - patch.reshape(1, -1)), axis=1))
-    print(z)
     if soft:
         # f_k = max{0, mu - z_k}
         features = np.clip(np.mean(z) - z, a_min=0, a_max=np.inf)
@@ -144,7 +172,7 @@ def extractFeatures(patch, D, soft=True):
         features[z.argmin()] = 1
     return features
 
-
+@profile
 def mapFeatures(image, D, patch_size=(16, 16), stride=1):
     n_features = len(D)
     p_x, p_y = patch_size
@@ -159,8 +187,8 @@ def mapFeatures(image, D, patch_size=(16, 16), stride=1):
             features[i, j, :] = extractFeatures(image[i: i + p_y, j: j + p_x], D)
     return features
 
-
-def pool(features, pool_size=(2, 2), type='max'):
+@profile
+def pool(features, pool_size=(4, 4), type='max'):
     feat_x, feat_y, feat_z = features.shape
     pool_x, pool_y = pool_size
     pooled_y = feat_y / pool_y
@@ -183,7 +211,14 @@ def pool(features, pool_size=(2, 2), type='max'):
 
 
 if __name__ == '__main__':
-    image = readImages(1)[0]
-    D = loadFeatureDict()
-    mapped_features = mapFeatures(image, D)
-    features = pool(mapped_features)
+    start_time = time.time()
+    images = readImages(100)
+    _logWithTimestamp('Images read')
+    D = buildFeatureDictionary(images, n_centroids=10)
+    _logWithTimestamp('Feature Dictionary ready')
+    features = np.zeros((len(images), len(D)*12*12))
+    for i, image in enumerate(images):
+        features[i, ...] = pool(mapFeatures(image, D)).reshape((1, -1))
+    _logWithTimestamp('Features mapped to images')
+    np.savetxt('/data/tidy/pooled_features_1024_5k.csv', features, delimiter=',')
+    _logWithTimestamp('Features saved, exiting')
