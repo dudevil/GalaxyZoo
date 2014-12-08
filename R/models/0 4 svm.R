a98f6208b3fe2b630d36247ff24c6f364f7208e8
Sergey Ovcharenko <ovcharenko.ser@gmail.com>
2014-12-08 16:44:42 +0300

    Switch to foreach parallelization. This should not copy memmory for each fork.

--- R/models/0 4 svm.R
+++ R/models/0 4 svm.R
@@ -1,4 +1,6 @@
-require('parallel')
+require('e1071')
+require('foreach')
+require('doMC')
 
 source(paste0(getwd(), '/R/data processing/0 1 read data.R'))
 source(paste0(getwd(), '/R/_utils.R'))
@@ -16,16 +18,33 @@ train.set <- galaxies[trainindex,]
 test.set <- galaxies[-trainindex,]
 
 # set up cluster
-cl <- makeCluster(detectCores())
+#cl <- makeCluster(detectCores())
 # export features to the cluster, they will we in scope of worker threads
-clusterExport(cl=cl, varlist=c("train.set", "test.set"))
+#clusterExport(cl=cl, varlist=c("train.set", "test.set"))
 
 #classes <- names(train.set[2002:ncol(train.set)])
 classes <- names(train.set[2002:2004])
 # a function that trains an svm model and makes predictions
 run.svm <- function(class){
-  library('e1071')
   # train svm with linear kernel
+  model.svm <- svm(x = train.set[,2:2001],
+                   y = train.set[,class],
+                   kernel='linear')
+  # save model
+  print(paste('Class', class, 'trained.'))
+  #save(model.svm, file=paste0(getwd(),'/data/models/svm_', class, '.rda'))
+  # predict on the test set
+  return(predict(model.svm, test.set[,2:2001]))
+}
+
+print('Starting SVM training and prediction...')
+# actually run the parallel jobs
+#results <- data.frame(mclapply(classes, run.svm,
+#                               mc.cores = getOption('mc.cores', 3L)))
+#stopCluster(cl)
+registerDoMC(3)
+
+res <- foreach(class = classes) %dopar% {
   model.svm <- svm(x = train.set[,2:2001],
                    y = train.set[,class],
                    kernel='linear')
@@ -33,13 +52,10 @@ run.svm <- function(class){
   print(paste('Class', class, 'trained.'))
   save(model.svm, file=paste0(getwd(),'/data/models/svm_', class, '.rda'))
   # predict on the test set
-  return(predict(model.svm, test.set[,2:2001]))
+  predict(model.svm, test.set[,2:2001])
 }
 
-# actually run the parallel jobs
-results <- data.frame(parLapply(cl, classes, run.svm))
-stopCluster(cl)
-
+results <- data.frame(res)
 print(paste('RMSE: ', rmse(as.matrix(results), test.set[,classes])))
 write.csv(results, row.names=FALSE,
           file = paste0(getwd(),"/data/tidy/svmpred_linear_untuned.csv"))
